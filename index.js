0bc0120b3849b10d2b4a51de7c40bf67a31d2300
Magnus Crafoord <magnus.crafoord@freespee.com>
2019-01-18 13:31:40 +0100

    from w3schools

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var Gpio = require('onoff').Gpio,
-    led = new Gpio(17, 'out');
- 
-var iv = setInterval(function(){
-	led.writeSync(led.readSync() === 0 ? 1 : 0)
-}, 500);
- 
-// Stop blinking the LED and turn it off after 5 seconds.
-setTimeout(function() {
-    clearInterval(iv); // Stop blinking
-    led.writeSync(0);  // Turn LED off.
-    led.unexport();    // Unexport GPIO and free resources
-}, 5000);
+var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(4, "out"); //use GPIO pin 4, and specify that it is output
+var blinkInterval = setInterval(blinkLED, 250); //run the blinkLED function every 250ms
+
+function blinkLED() {
+  //function to start blinking
+  if (LED.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+function endBlink() {
+  //function to stop blinking
+  clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  LED.unexport(); // Unexport GPIO to free resources
+}
+
+setTimeout(endBlink, 5000); //stop blinking after 5 seconds
