9d948f5e116a5167e1a92e4ca86f0a6ec65bc7ed
Crafoord <magnus.crafoord@gmail.com>
2019-01-18 12:31:31 +0100

    blink code

---
+++ index.js
@@ -0,0 +1,23 @@
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+var Gpio = require('onoff').Gpio,
+    led = new Gpio(17, 'out');
+ 
+var iv = setInterval(function(){
+	led.writeSync(led.readSync() === 0 ? 1 : 0)
+}, 500);
+ 
+// Stop blinking the LED and turn it off after 5 seconds.
+setTimeout(function() {
+    clearInterval(iv); // Stop blinking
+    led.writeSync(0);  // Turn LED off.
+    led.unexport();    // Unexport GPIO and free resources
+}, 5000);
